5ab7516bed98dcdbfd048f1abfa88bb0481903b1
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2026-04-18 07:37:13 +0200

    Cursor encoder package definition fix

--- package/main/src/isCursor.ts
+++ package/main/src/isCursor.ts
@@ -1,7 +1,6 @@
 import { Cursor } from "./Cursor";
 
 export function isCursor<T = unknown>(value: unknown): value is Cursor<T> {
-	// eslint-disable-next-line no-null/no-null
 	return (
 		typeof value === "object" &&
 		value !== null &&
